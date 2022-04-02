b170b6dc2f828e41ad063cb66bceea4981ea4217
VaNomad <sjur.hassel@gmail.com>
2022-04-02 13:33:12 +0100

    Question 1 & 2 finished

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const cats = [
+  {
+      name: "Blob",
+      age: 10
+  },
+  {
+      name: "Harold",
+  },
+  {
+      name: "Blurt",
+      age: 21
+  }
+];
+
+// ------------------------------------------- Question 1
+const cat = { complain: console.log("Meow") };
+
+// ------------------------------------------- Question 2
+const heading = document.querySelector("h3");
+heading.innerHTML = "Updated heading";
+
+// ------------------------------------------- Question 3
+
+
